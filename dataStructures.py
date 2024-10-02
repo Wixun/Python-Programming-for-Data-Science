603086f77626f6920dae3f26d4b48abb800fc118
Eren KIRMIZI <125762151+Wixun@users.noreply.github.com>
2024-10-02 16:36:31 +0300

    dataStructures.py

---
+++ dataStructures.py
@@ -0,0 +1,45 @@
+# Sayılar: Integer (Tam Sayı)
+x = 46
+print(x)                # 46
+print(type(x))          # <class 'int'>
+
+# Sayılar: Float (Ondalık Sayı)
+x = 10.3
+print(x)                # 10.3
+print(type(x))          # <class 'float'>
+
+# Sayılar: Complex (Karmaşık Sayı)
+x = 2j + 1
+print(x)                # (1+2j)
+print(type(x))          # <class 'complex'>
+
+# String (Metin)
+x = "Hello ai era"
+print(x)                # Hello ai era
+print(type(x))          # <class 'str'>
+
+# Boolean (Mantıksal Değer)
+print(True)             # True
+print(False)            # False
+print(type(True))       # <class 'bool'>
+print(5 == 4)           # False
+
+# Liste (List)
+x = ["btc", "eth", "xrp"]
+print(x)                # ['btc', 'eth', 'xrp']
+print(type(x))          # <class 'list'>
+
+# Sözlük (Dictionary)
+x = {"name": "Peter", "Age": 36}  # Sözlük key-value (anahtar-değer) çiftlerinden oluşur.
+print(x)                # {'name': 'Peter', 'Age': 36}
+print(type(x))          # <class 'dict'>
+
+# Demet (Tuple)
+x = ("python", "ml", "ds")
+print(x)                # ('python', 'ml', 'ds')
+print(type(x))          # <class 'tuple'>
+
+# Set (Küme)
+x = {"python", "ml", "ds"}
+print(x)                # {'python', 'ml', 'ds'} (sırasız ve tekrarsız)
+print(type(x))          # <class 'set'>
